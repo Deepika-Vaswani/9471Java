40fe894f1e83c84c2b553e18488172dc2b057d42
Deepika-Vaswani <86174178+Deepika-Vaswani@users.noreply.github.com>
2021-12-06 03:03:18 +0530

    Add files via upload

---
+++ AnudipPractical/Evenodd.java
@@ -0,0 +1,11 @@
+public class Evenodd{
+	public static void main(String args[]){
+	 int a = 54;
+	 if(a%2==0){
+	  System.out.println("Number is Even");
+	  }
+	  else{
+	 	 System.out.println("Number is Odd");
+	  	}
+	}
+}
