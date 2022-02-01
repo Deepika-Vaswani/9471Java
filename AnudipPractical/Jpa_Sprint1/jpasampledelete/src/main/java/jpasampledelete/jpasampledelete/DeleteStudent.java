64dcde821a2ae6e312a0e4156960d87cfe57c2c8
Deepika-Vaswani <86174178+Deepika-Vaswani@users.noreply.github.com>
2022-02-01 22:04:31 +0530

    Add files via upload

---
+++ AnudipPractical/Jpa_Sprint1/jpasampledelete/src/main/java/jpasampledelete/jpasampledelete/DeleteStudent.java
@@ -0,0 +1,27 @@
+package jpasampledelete.jpasampledelete;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import jpasampledelete.jpasampledelete.entity.StudentEntity;
+
+/**
+ * Hello world!
+ *
+ */
+public class DeleteStudent 
+{
+    public static void main( String[] args )
+    {
+    	EntityManagerFactory emf=Persistence.createEntityManagerFactory("Student_details");  
+	    EntityManager em=emf.createEntityManager();  
+	em.getTransaction().begin();  
+  
+	    StudentEntity s=em.find(StudentEntity.class,102);  
+	em.remove(s);  
+	em.getTransaction().commit();  
+	emf.close();  
+	em.close();
+    }
+}
