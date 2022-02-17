e1ebbdaa359fbdbb4f31db829add134471a2f3eb
Deepika-Vaswani <86174178+Deepika-Vaswani@users.noreply.github.com>
2022-02-17 13:37:21 +0530

    Add files via upload

---
+++ AnudipPractical/Spring2/SpringProject_Demo/src/SpringProject_Demo/Test.java
@@ -0,0 +1,15 @@
+package SpringProject_Demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+	      
+	    Student student=(Student)ac.getBean("studentbean");  
+			    student.displayInfo(); 
+			    ((ClassPathXmlApplicationContext) ac).close();
+}
+}
